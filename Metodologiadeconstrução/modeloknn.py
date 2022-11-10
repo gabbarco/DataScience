7027063bd8d24fa5faacc3274453e492178afb40
Gabriel Barco <gabborges37@gmail.com>
2022-11-10 15:05:09 -0300

    MetodologiaPreditiva 1.0

---
+++ Metodologiadeconstrução/modeloknn.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.spatial import distance
+from sklearn.neighbors import KNeighborsClassifier
+
+# ler o arquivo 
+pesquisa = pd.read_csv("C:/Users/46456334810/Documents/CiênciaDeDados/Metodologiadeconstrução/bank.csv", sep=";")
+
+df = pesquisa.drop(['age','balance','day','month','duration','campaign','pdays','previous'], axis=1)
+
+treinamento = df[0:3021]
+
+rotulo = treinamento.get('y')
+teste = df[3021:]
+
+treinamento= treinamento.drop(['y'],axis=1)
+X_train = pd.get_dummies(treinamento,drop_first=True)
+
+classificador = KNeighborsClassifier(n_neighbors = 8)#instanciar o modelo
+classificador.fit(X_train,rotulo)#treinar o modelo
+
+teste= teste.drop(['y'], axis=1)
+
+X_test = pd.get_dummies(teste,drop_first=True)
+
+from sklearn.metrics import confusion_matrix
+
+y_true= teste = df.y[3021:]
+
+y_pred= (classificador.predict(X_test))
+
+mcNB = confusion_matrix(y_true, y_pred)
+
+print(mcNB)
+
+acur = (mcNB[0,0] + mcNB[1,1]) / len(y_true)
+
+print("Acurácia knn: ",acur*100, "%")
