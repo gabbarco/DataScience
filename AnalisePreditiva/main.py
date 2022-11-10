e64f02beabd9e49a158e8cb12defee91d8f5b2e9
Gabriel Barco <gabborges37@gmail.com>
2022-11-10 15:04:21 -0300

    AnalisePreditiva 1.0

---
+++ AnalisePreditiva/main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.spatial import distance
+from sklearn.neighbors import KNeighborsClassifier
+
+# ler o arquivo 
+pesquisa = pd.read_csv("C:/Users/46456334810/Documents/CiênciaDeDados/AnalisePreditiva/parkinsons.data", sep=",", decimal=".")
+print(pesquisa)
+df = pesquisa.drop(['name'], axis=1)
+
+treinamento1 = df[0:24]
+print(treinamento1)
+treinamento2= df[30:len(df)]
+print(treinamento2)
+treinamento= pd.concat([treinamento1,treinamento2])
+print(treinamento)
+rotulo = treinamento.get('status')
+print(rotulo)
+teste = df[24:30]
+print(teste)
+
+treinamento= treinamento.drop(['status'], axis=1)
+
+classificador = KNeighborsClassifier(n_neighbors = 33)#instanciar o modelo
+
+classificador.fit(treinamento,rotulo)#treinar o modelo
+
+print("Status do paciente isolado")
+teste= teste.drop(['status'], axis=1)
+print(classificador.predict(teste))#predição dos dados teste
