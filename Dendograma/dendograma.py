d0addc7b19a20a5d78f6e99aa2a63052cec55cee
46456334810 <gabborges37@gmail.com>
2022-11-17 15:18:21 -0300

    Dendograma 2.0

--- Dendograma/dendograma.py
+++ Dendograma/dendograma.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.cluster.hierarchy import dendrogram
 from scipy.spatial.distance import pdist
 
-pesquisa = pd.read_csv("C:/Users/46456334810/DataScience/Dendograma/processed.cleveland.data", sep=",",decimal=".")
+pesquisa = pd.read_csv("", sep=",",decimal=".")  #Leitura do arquivo
 df= pesquisa.drop(["num","ca","thal"],axis=1)
 
 d = pdist(df,metric='euclidean')
